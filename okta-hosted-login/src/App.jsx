8f14fc8803293760b04a0fa43f129fa97a508a2a
petervucic1 <102125647+petervucic1@users.noreply.github.com>
2023-09-25 20:57:33 +1000

    Update App.jsx

--- okta-hosted-login/src/App.jsx
+++ okta-hosted-login/src/App.jsx
@@ -17,7 +17,7 @@ import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
 import { Container } from 'semantic-ui-react';
 import config from './config';
 import Home from './Home';
-//import searchuser from './searchuser';
+import searchuser from './searchuser';
 import Messages from './Messages';
 import Navbar from './Navbar';
 import Profile from './Profile';
@@ -66,6 +66,7 @@ const App = () => {
           <Route path="/login/callback" component={LoginCallback} />
           <SecureRoute path="/messages" component={Messages} />
           <SecureRoute path="/profile" component={Profile} />
+          <SecureRoute path="/searchuser" component={Profile} />
         </Switch>
       </Container>
     </Security>
